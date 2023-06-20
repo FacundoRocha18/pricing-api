f64a3f2709da328d833888b4521ec2cd11915c9a
Facundo Rocha <facundo.rocha@outlook.com>
2023-06-20 17:56:44 -0300

    TODO: refactor email exists test

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
   }
 
   async signin(email: string, password: string) {
-    const user = await this.usersService.findByEmail(email);
+    const [user] = await this.usersService.find({ email });
 
     if (!user) {
       throw new NotFoundException(
--- src/auth/test/auth.service.spec.ts
+++ src/auth/test/auth.service.spec.ts
@@ -2,17 +2,20 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '../auth.service';
 import { UsersService } from '../../users/users.service';
 import { User } from 'src/users/user.entity';
-import { FindOptionsWhere } from 'typeorm';
 import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
   let usersServiceMock: Partial<UsersService>;
+  const userDataSample = {
+    email: 'test@test.com',
+    name: 'Test',
+    password: 'Password1234!',
+  };
 
   beforeEach(async () => {
     usersServiceMock = {
-      find: (findOptions: FindOptionsWhere<User>): Promise<User[]> =>
-        Promise.resolve([]),
+      find: (): Promise<User[]> => Promise.resolve([]),
       create: ({ email, name, password }) =>
         Promise.resolve({
           email,
@@ -39,15 +42,11 @@ describe('AuthService', () => {
   });
 
   it('should create a new user with a salted and hashed password', async () => {
-    const user = await service.signup({
-      email: 'test@test.com',
-      name: 'Test',
-      password: 'Password1234!',
-    });
+    const user = await service.signup(userDataSample);
 
     const [salt, hash] = user.password.split('.');
 
-    expect(user.password).not.toEqual('Password1234!');
+    expect(user.password).not.toEqual(userDataSample.password);
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
   });
@@ -63,12 +62,25 @@ describe('AuthService', () => {
         } as User,
       ]);
 
+    await expect(service.signup(userDataSample)).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws an error if user tries to sign in with an unused email', async () => {
+    usersServiceMock.find = () => {
+      return Promise.resolve([
+        {
+          id: 'bf229f23-48f6-4dbc-b44d-2732f855ec7d',
+          email: 'test@test.com',
+          name: 'Test',
+          password: 'Password1234!',
+        } as User,
+      ]);
+    };
+
     await expect(
-      service.signup({
-        email: 'test@test.com',
-        name: 'Test',
-        password: 'Password1234!',
-      }),
+      service.signin(userDataSample.email, userDataSample.password),
     ).rejects.toThrow(BadRequestException);
   });
 });
