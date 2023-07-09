cc16d6fb8dbc104183d29470994274564a6ddb46
Facundo Rocha <facundo.rocha@outlook.com>
2023-07-08 23:15:42 -0300

    Fixed wrong import paths

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -4,9 +4,9 @@ import { Report } from './report.entity';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CurrentUserInterceptor } from 'src/users/interceptors/current-user.interceptor';
-import { User } from 'src/users/user.entity';
-import { UsersService } from 'src/users/users.service';
+import { CurrentUserInterceptor } from '../users/interceptors/current-user.interceptor';
+import { User } from '../users/user.entity';
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [
